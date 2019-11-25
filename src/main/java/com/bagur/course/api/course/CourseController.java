5feafc5e406b5b3d9410a031b369f9d30aa3ff5a
pugmark <pramukhdbagur@gmail.com>
2019-11-25 17:15:43 -0600

    Added courses API to the project

---
+++ src/main/java/com/bagur/course/api/course/CourseController.java
@@ -0,0 +1,51 @@
+package com.bagur.course.api.course;
+
+import java.util.List;
+
+import com.bagur.course.api.topic.Topic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    // GET - gets all Courses
+    @RequestMapping("/topics/{topicId}/courses")
+    public List<Course> getAllCourses(@PathVariable String topicId) {
+        return courseService.getAllCourses(topicId);
+    }
+
+    // GET - Gets one Course
+    @RequestMapping("/topics/{topicId}/courses/{courseId}")
+    public Course getTopic(@PathVariable String courseId) {
+        return courseService.getCourse(courseId);
+    }
+
+    // POST - Adds a Course
+    @RequestMapping(method = RequestMethod.POST, value = "/topics/{topicId}/courses")
+    public void addCourse(@RequestBody Course course, @PathVariable String topicId) {
+        course.setTopic(new Topic(topicId, "", ""));
+        courseService.addCourse(course);
+    }
+
+    // PUT - Updates a Course
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{topicId}/courses/{courseId}")
+    public void updateCourse(@RequestBody Course course, @PathVariable String topicId) {
+        course.setTopic(new Topic(topicId, "", ""));
+        courseService.updateCourse(course);
+    }
+    
+    // DELETE - Deletes a Course
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{topicId}/courses/{courseId}")
+    public void deleteCourse(@PathVariable String courseId) {
+        courseService.deleteCourse(courseId);
+    }
+}
