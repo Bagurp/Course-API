40fcc6fb13a63f1e98923e0054e71813bf6d0ed0
pugmark <pramukhdbagur@gmail.com>
2020-01-03 16:25:15 -0600

    Customize auto-generated Swagger definitions

--- src/main/java/com/bagur/course/api/SpringFoxConfig.java
+++ src/main/java/com/bagur/course/api/SpringFoxConfig.java
@@ -6,8 +6,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,11 +21,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SpringFoxConfig extends WebMvcConfigurerAdapter {                                    
     @Bean
     public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-        .select()                                  
-        .apis(RequestHandlerSelectors.any())              
-        .paths(PathSelectors.any())                          
-        .build();                                           
+        return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.bagur"))
+        .paths(PathSelectors.any())
+        .build()
+        .apiInfo(getApiInfo());
     }
 
     @Override
@@ -32,5 +36,16 @@ public class SpringFoxConfig extends WebMvcConfigurerAdapter {
     
         registry.addResourceHandler("/webjars/**")
         .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot REST API - Course API")
+                .description("\"Simple Spring Boot REST Api for Topics and Courses\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(new Contact("Pramukh Bagur", "https://www.linkedin.com/in/pramukh-bagur", "pramukhdbagur@gmail.com"))
+                .build();
     }
 }
