64d2b515afc44847ab034f034ba1c487ae2090d1
pugmark <pramukhdbagur@gmail.com>
2020-01-03 15:04:50 -0600

    Add Swagger UI to project

--- src/main/java/com/bagur/course/api/course/CourseController.java
+++ src/main/java/com/bagur/course/api/course/CourseController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.bagur.course.api.topic.Topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
